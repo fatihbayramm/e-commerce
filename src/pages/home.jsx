9cd435821016eebaa5d9e9bb9052ba11041ac480
fatih bayram <fatihbayramybs@gmail.com>
2024-09-19 15:38:28 +0300

    loading animation added.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,15 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import "../css/home.css";
 import GetProducts from "../components/get-products";
+import Loading from "../components/loading";
 
 function Home() {
   return (
     <div className="home">
       <Container maxWidth="xl" style={{ fontFamily: "Arial" }}>
         <Header />
+        <Loading />
         <GetProducts />
-
         <Footer />
       </Container>
     </div>
