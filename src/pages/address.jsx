6fa58bc207c4ab4f5997c3224dbd567d65373d5d
fatih bayram <fatihbayramybs@gmail.com>
2024-10-23 21:32:49 +0300

    address page

--- src/pages/address.jsx
+++ src/pages/address.jsx
@@ -38,67 +38,109 @@ function Address() {
     dispatch(getTownships());
   }, [dispatch]);
 
-  const { address } = useSelector((store) => store.address);
+  const { address, loading } = useSelector((store) => store.address);
 
   return (
     <div>
       <Header />
-      <Container maxWidth="xl" className="login-container">
-        <form onSubmit={handleSubmit}>
+      <Container maxWidth="xl" className="address-container">
+        <form onSubmit={handleSubmit} className="address-form">
           <h1>Address Information</h1>
 
           <div className="address-box">
             <input
               type="text"
               name="name"
+              className="address-input"
               value={values.name}
               onChange={handleChange}
               placeholder="Enter your address title"
             />
+            {errors.name ? (
+              <div className="input-error show">{errors.name}</div>
+            ) : (
+              <div className="input-error"></div>
+            )}
           </div>
           <div className="address-box">
             <select
               name="country"
+              className="address-select"
               value={values.country}
               onChange={handleChange}
             >
-              <option>Choose</option>
+              <option className="address-choose">Choose</option>
 
               {address.countries.map((country) => (
                 <option key={country.id}>{country.name}</option>
               ))}
               {console.log(values.country)}
             </select>
+            {errors.country ? (
+              <div className="input-error show">{errors.country}</div>
+            ) : (
+              <div className="input-error"></div>
+            )}
           </div>
           <div className="address-box">
-            <select name="city" value={values.city} onChange={handleChange}>
-              <option>Choose</option>
+            <select
+              name="city"
+              className="address-select"
+              value={values.city}
+              onChange={handleChange}
+            >
+              <option className="address-choose">Choose</option>
 
               {address.cities.map((city) => (
                 <option key={city.id}>{city.name}</option>
               ))}
               {console.log(values.city)}
             </select>
+            {errors.city ? (
+              <div className="input-error show">{errors.city}</div>
+            ) : (
+              <div className="input-error"></div>
+            )}
           </div>
           <div className="address-box">
-            <select name="township" value={values.city} onChange={handleChange}>
-              <option>Choose</option>
+            <select
+              name="township"
+              className="address-select"
+              value={values.township}
+              onChange={handleChange}
+            >
+              <option className="address-choose">Choose</option>
 
               {address.townships.map((township) => (
                 <option key={township.id}>{township.name}</option>
               ))}
               {console.log(values.township)}
             </select>
+            {errors.township ? (
+              <div className="input-error show">{errors.township}</div>
+            ) : (
+              <div className="input-error"></div>
+            )}
           </div>
           <div className="address-box">
             <input
               type="text"
               name="text"
+              className="address-input"
               value={values.text}
               onChange={handleChange}
               placeholder="Enter your specific address."
             />
+            {errors.text ? (
+              <div className="input-error show">{errors.text}</div>
+            ) : (
+              <div className="input-error"></div>
+            )}
           </div>
+
+          <button className="address-save-btn">
+            {loading ? "Saving ..." : "Save Address"}
+          </button>
         </form>
       </Container>
 
--- src/pages/auth/schemas/address-form-schemas.jsx
+++ src/pages/auth/schemas/address-form-schemas.jsx
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const addressFormSchemas = yup.object().shape({
-  name: yup.string().email().required("Enter a valid email address."),
+  name: yup.string().required("Address title is required."),
   country: yup.string().required("Country field is required."),
   city: yup.string().required("City field is required."),
   township: yup.string().required("Township field is required."),
